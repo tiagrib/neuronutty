6c0c1f2f230058763700238d0cc4c9d2ceebc7e4
Tiago Ribeiro <me@tiagoribeiro.pt>
2024-05-27 15:17:09 +0100

    Fairmotion controller storing and playing own computed poses on recompute

--- nnutty/controllers/fairmotion_torch_model_controller.py
+++ nnutty/controllers/fairmotion_torch_model_controller.py
@@ -7,6 +7,7 @@ from nnutty.controllers.anim_file_controller import AnimFileController
 from nnutty.controllers.character_controller import CharCtrlType, CharacterSettings
 from nnutty.controllers.uncached_anim_controller import UncachedAnimController
 from fairmotion.tasks.motion_prediction import generate, utils
+from fairmotion.core.motion import Pose
 from fairmotion.ops import conversions
 
 
@@ -15,12 +16,13 @@ class FairmotionModelController(UncachedAnimController):
         super().__init__(ctrl_type=CharCtrlType.MODEL, settings=settings)
         self.orig_anim_length = 0.0
         self.in_prediction = False
-        self.anim_file_ctrl = AnimFileController(settings=settings)
+        self.computed_poses = []
+        self.fps = 1.0
         self.load_model(model_path=model)
 
     def load_model(self, model_path:str):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.num_predictions = 200
+        self.num_predictions = 90
         self.num_dim = 72
         hidden_dim = 1024
         num_layers = 1
@@ -51,14 +53,18 @@ class FairmotionModelController(UncachedAnimController):
         self.model.eval()
 
     def load_anim_file(self, filename:str):
-        self.anim_file_ctrl.load_anim_file(filename)
-        self.orig_anim_length = self.anim_file_ctrl.end_time
-        self.recompute_prediction()
+        anim_file_ctrl = AnimFileController(settings=self.settings)
+        anim_file_ctrl.load_anim_file(filename)
+        self.full_anim_length = anim_file_ctrl.end_time
+        self.reference_anim_length = self.full_anim_length
+        self.fps = anim_file_ctrl.fps
+        self.recompute_prediction(anim_file_ctrl)
 
-    def recompute_prediction(self):
+    def recompute_prediction(self, anim_file_ctrl):
         logging.info("Running model")
-        input_motion = self.anim_file_ctrl.motion.rotations()
-        # apply conversions.R2A() to each element of the array's axis 2 using numpy        
+        self.reference_anim_length -= self.num_predictions/anim_file_ctrl.fps
+        self.num_ref_frames = anim_file_ctrl.motion.num_frames() - self.num_predictions
+        input_motion = anim_file_ctrl.motion.rotations()[:self.num_ref_frames]
         input_motion = conversions.R2A(input_motion[:,:,:])
         input_motion = input_motion.reshape(1, -1, self.num_dim)
         input_motion = torch.from_numpy(input_motion)
@@ -67,26 +73,38 @@ class FairmotionModelController(UncachedAnimController):
             .to(device="cpu")
             .numpy()
         )
-        self.anim_file_ctrl.digest_motion(pred_seq, append=True)
+
+        self.computed_poses.clear()
+        for i in range(self.num_ref_frames):
+            self.computed_poses.append(anim_file_ctrl.motion.get_pose_by_frame(i))
+        for i in range(self.num_predictions):
+            poses = conversions.A2T(pred_seq[0].reshape(pred_seq[0].shape[0], pred_seq[0].shape[1] // 3, 3))
+            self.computed_poses.append(Pose(anim_file_ctrl.motion.skel, poses[i]))
+        self.reset()
+
 
     def reset(self):
         super().reset()
-        self.anim_file_ctrl.reset()
+        self.cur_time = 0.0
 
     def advance_time(self, dt, params=None):
-        self.anim_file_ctrl.advance_time(dt, params)
-        self.cur_time = self.anim_file_ctrl.cur_time
-        if self.cur_time > self.orig_anim_length:
-            if not self.in_prediction:
-                logging.info("Displaying prediction")
-                self.settings.color = np.array([173, 130, 50, 255]) / 255.0  # orange-red
-            self.in_prediction = True
-        else:
-            if self.in_prediction:
-                logging.info("Displaying original animation")
-                self.settings.color = np.array([85, 160, 173, 255]) / 255.0  # blue
-            self.in_prediction = False
-        self.compute(dt, params)
+        if self.computed_poses:
+            self.cur_time += dt
+            if self.cur_time > self.full_anim_length:
+                self.cur_time = 0.0
+            if self.cur_time > self.reference_anim_length:
+                if not self.in_prediction:
+                    logging.info("Displaying prediction")
+                    self.settings.color = np.array([173, 130, 50, 255]) / 255.0  # orange-red
+                self.in_prediction = True
+            else:
+                if self.in_prediction:
+                    logging.info("Displaying original animation")
+                    self.settings.color = np.array([85, 160, 173, 255]) / 255.0  # blue
+                self.in_prediction = False
+            self.compute(dt, params)
     
     def compute(self, dt=None, params={}):
-        self.pose = self.anim_file_ctrl.get_pose()
+        if self.computed_poses:
+            pose_i = min(int(self.cur_time * self.fps), len(self.computed_poses) - 1)
+            self.pose = self.computed_poses[pose_i]
