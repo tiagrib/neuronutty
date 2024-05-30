2aa922782c1fd257bd6db506cc306d9083df9050
Tiago Ribeiro <me@tiagoribeiro.pt>
2024-05-30 18:03:58 +0100

    Update train.ps1

--- scripts/train.ps1
+++ scripts/train.ps1
@@ -1,11 +1,13 @@
 $elements = Get-ChildItem -Path "Z:\preprocessed" -Directory | ForEach-Object { $_.Name }
 $architectures = "seq2seq","transformer","transformer_encoder"
 foreach ($elem in $elements) {
-    $saveModelPath = "Z:\models\${elem}_${architecture}"
-    $preprocessedPath = "Z:\preprocessed\$elem\aa"
-    if (-not (Test-Path $saveModelPath)) {
-        python -m fairmotion.tasks.motion_prediction.training --save-model-path $saveModelPath --preprocessed-path $preprocessedPath --epochs 100 --device cuda --save-model-frequency 5  --architecture $architecture
-    } else {
-        Write-Host "Skipped existing model '$saveModelPath'."
+    foreach ($architecture in $elements) {
+        $saveModelPath = "Z:\models\${elem}_${architecture}"
+        $preprocessedPath = "Z:\preprocessed\$elem\aa"
+        if (-not (Test-Path $saveModelPath)) {
+            python -m fairmotion.tasks.motion_prediction.training --save-model-path $saveModelPath --preprocessed-path $preprocessedPath --epochs 100 --device cuda --save-model-frequency 5 --architecture $architecture
+        } else {
+            Write-Host "Skipped existing model '$saveModelPath'."
+        }
     }
 }
