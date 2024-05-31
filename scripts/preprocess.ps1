9850232cbc7c96be79dd2cc93bb24f35934fa680
Tiago Ribeiro <me@tiagoribeiro.pt>
2024-05-31 12:45:16 +0100

    Fix scripts

--- scripts/preprocess.ps1
+++ scripts/preprocess.ps1
@@ -1,14 +1,18 @@
 $sourceDir = "Z:\mocap\AMASS\Synthetic_60FPS"
 $splitDir = "${sourceDir}_fnames"
 $elements = Get-ChildItem -Path $sourceDir -Directory | ForEach-Object { $_.Name }
-$elements += "."
+$elements += ""
 foreach ($elem in $elements) {
+    Write-Host "Preprocessing dataset '$elem'..."
     $inputDir = "${sourceDir}\${elem}"
-    if ($elem -eq ".") {
-        $elem = "full"
+    Write-Host "Input directory: $inputDir"
+    if ($elem -eq "") {
+        $elem = "AMASS_full"
+        $inputDir = $sourceDir
     }
     $outputDir = "Z:\preprocessed\${elem}"    
-    if (-not (Test-Path $inputDir)) {
+    Write-Host "Output directory: $outputDir"
+    if (-not (Test-Path $outputDir)) {
         python -m fairmotion.tasks.motion_prediction.preprocess --input-dir $inputDir --output-dir $outputDir --split-dir $splitDir --rep aa
     } else {
         Write-Host "Skipped existing preprocessed dataset '$inputDir'."
--- scripts/train.ps1
+++ scripts/train.ps1
@@ -1,9 +1,13 @@
 $elements = Get-ChildItem -Path "Z:\preprocessed" -Directory | ForEach-Object { $_.Name }
 $architectures = "seq2seq","transformer","transformer_encoder"
 foreach ($elem in $elements) {
+    Write-Host "Preprocessing dataset '$elem'..."
     foreach ($architecture in $architectures) {
+        Write-Host "Preprocessing architecture '$architecture'..."
         $saveModelPath = "Z:\models\${elem}_${architecture}"
         $preprocessedPath = "Z:\preprocessed\$elem\aa"
+        Write-Host "Save model path: $saveModelPath"
+        Write-Host "Preprocessing Path: $preprocessedPath"
         if (-not (Test-Path $saveModelPath)) {
             python -m fairmotion.tasks.motion_prediction.training --save-model-path $saveModelPath --preprocessed-path $preprocessedPath --epochs 100 --device cuda --save-model-frequency 5 --architecture $architecture
         } else {
